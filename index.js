250ceedb83f44158c0e52ed006936933c1128688
Harsh <soni.hs661@gmail.comm>
2022-04-15 16:11:59 +0530

    GG

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose=require('./config/mongoose');
+const mongoose = require("./config/mongoose");
 app.use(cors({}));
 app.use(bodyParser.json());
 app.use(express.urlencoded());
@@ -12,7 +12,9 @@ const chatServer = http.createServer(app);
 const chatSockets = require("./config/chat_socket").chatSockets(chatServer);
 chatServer.listen(5000);
 console.log("chatServer is listening on port 5000");
-
+app.get("/", (req, res) => {
+  res.send("SEND");
+});
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
